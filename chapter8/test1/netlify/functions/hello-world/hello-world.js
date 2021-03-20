de09f1481272decad9fa11998b0c48f24c67a99b
Raymond Camden <raymondcamden@gmail.com>
2021-03-20 10:37:36 -0500

    c8 work

---
+++ chapter8/test1/netlify/functions/hello-world/hello-world.js
@@ -0,0 +1,19 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+//const handler = async (event) => {
+const handler = async function (event) {
+
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
